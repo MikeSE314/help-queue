dc68d8ada864ec2e78adf526bcba3216f32a8cc4
MikeSE314 <micheal.erickson3.14@gmail.com>
2020-03-02 14:41:48 -0700

    hide password in .env

--- help_queue/db_connector.js
+++ help_queue/db_connector.js
@@ -1,5 +1,11 @@
 let mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://db_user:8mnjhnm48d5dw62hnb5j@help-queue-p0xtr.mongodb.net/help_queue?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
+require('dotenv').config();
+
+let password = process.env.DB_PW || ""
+
+console.log("using password " + password)
+
+mongoose.connect("mongodb+srv://db_user:" + password + "@help-queue-p0xtr.mongodb.net/help_queue?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
 
 module.exports = mongoose
