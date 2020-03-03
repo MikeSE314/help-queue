7ab6a93b0b21ee66017be17cdceabac145cfa171
MikeSE314 <micheal.erickson3.14@gmail.com>
2020-03-03 12:42:55 -0700

    big changes

--- help_queue/models/User.js
+++ help_queue/models/User.js
@@ -3,33 +3,16 @@
 var db = require("../db_connector")
 
 let userSchema = new db.Schema({
-  username: String,
+  netid: String,
   firstname: String,
   lastname: String,
-  s_salted_password: String,
-  salt_c: String,
-  salt_s: String,
-  admin: Boolean,
+}, {
+  collection: "students"
 })
 
 // sizzledashviewlessoversweet
 
 let User = db.model("User", userSchema)
-User.isValid = (body) => {
-  if (!body.username) {
-    return false
-  }
-  if (!body.firstname) {
-    return false
-  }
-  if (!body.lastname) {
-    return false
-  }
-  if (!body.password) {
-    return false
-  }
-  return true
-}
 
 module.exports = User
 
