ff40b0408d8578698ab03324cf5ee82a070ae71e
MikeSE314 <micheal.erickson3.14@gmail.com>
2020-03-14 12:54:40 -0600

    change to partials view layout

--- help_queue/controllers/index.js
+++ help_queue/controllers/index.js
@@ -11,7 +11,7 @@ let secondary = process.env.SECONDARY || getInt()
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  res.render("index", {admin: req.session.authorized, p: primary, s: secondary})
+  res.render("shared/layout", {view: "../index", title: "Home", admin: req.session.authorized, p: primary, s: secondary})
 })
 
 router.get("/login", async (req, res, next) => {
@@ -19,16 +19,20 @@ router.get("/login", async (req, res, next) => {
     res.redirect("/")
     return
   }
-  res.render("login", {p: primary, s: secondary})
+  // res.render("login", {p: primary, s: secondary})
+  res.render("shared/layout", {view: "../login", title: "Login", admin: req.session.authorized, p: primary, s: secondary})
 })
 
+/*
 router.get("/register", async (req, res, next) => {
   if (req.session.authorized) {
     res.redirect("/")
     return
   }
-  res.render("login", {p: primary, s: secondary})
+  // res.render("login", {p: primary, s: secondary})
+  res.render("shared/layout", {view: "login.ejs", title: "Login", admin: req.session.authorized, p: primary, s: secondary})
 })
+*/
 
 router.get("/logout", async (req, res, next) => {
   req.session.authorized = false
@@ -36,7 +40,8 @@ router.get("/logout", async (req, res, next) => {
 })
 
 router.get("/tutorial", async (req, res, next) => {
-  res.render("tutorial", {p: primary, s: secondary})
+  // res.render("tutorial", {p: primary, s: secondary})
+  res.render("shared/layout", {view: "../tutorial", title: "Tutorial", admin: req.session.authorized, p: primary, s: secondary})
 })
 
 module.exports = router
